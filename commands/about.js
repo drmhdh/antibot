c8fa9435e916bae7e808912720cf5a840e35cd84
Marco T.C. (BadWolf) <marco+github@lunawolf.es>
2019-08-06 21:48:31 +0200

    ESP

--- commands/about.js
+++ commands/about.js
@@ -5,7 +5,7 @@ const { onlyPublic } = require('../middlewares')
 composer.command('about',
   Composer.branch(onlyPublic,
     (ctx, next) => next(),
-    Composer.reply('Bot can restrict bot-like user via captha and/or detect and delete ads in chat where it works.', {
+    Composer.reply('Este bot puede restringir los usuarios-bot que envían spam con link y pide confirmación de que no se es un BOT para poder estar en el grupo, si es spam o bot elimina el contenido automaticamente.<br>Bot can restrict bot-like user via captha and/or detect and delete ads in chat where it works.', {
       parse_mode: 'HTML',
       disable_web_page_preview: true
     })
